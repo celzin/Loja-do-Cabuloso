e0c282692e0f343be06dddb424e764260370c173
Pedro Dias <phpdias@outlook.com>
2023-06-22 15:15:55 -0300

    register user

--- js/controle.js
+++ js/controle.js
@@ -1,10 +1,20 @@
 var pagina = -1;
 var elementos = 0;
 
-function Pessoa(nome, email, telefone) {
-    this.nome = nome;
-    this.email = email;
-    this.telefone = telefone;
+class Pessoa {
+    constructor(email, nome, sobrenome, nick, telefone, cep, estado, endereco, cidade, mensagem) {
+        this.email = email;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.nick = nick;
+        this.telefone = telefone;
+
+        this.cep = cep;
+        this.estado = estado;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.mensagem = mensagem;
+    }
 }
 
 var listaPessoas = [];
@@ -27,26 +37,64 @@ async function Cadastrar() {
     //    console.log(doc.id, ': ', dados.nome, dados.email, dados.telefone)
     //});
 
-    var nome = document.getElementById("nomeid");
-    var telefone = document.getElementById("telefoneid");
+    // CADASTRO PESSOA
     var email = document.getElementById("emailid");
+    var nome = document.getElementById("firstid");
+    var sobrenome = document.getElementById("lastid");
+    var nick = document.getElementById("nickid");
+    var telefone = document.getElementById("numberid");
 
-    var aux_Nome = nome.value;
-    var aux_Telefone = telefone.value;
+    //ENTREGA PESSOA
+    var cep = document.getElementById("zipid");
+    var estado = document.getElementById("regionid");
+    var endereco = document.getElementById("addid");
+    var cidade = document.getElementById("cityid");
+    var mensagem = document.getElementById("messageid");
+
     var aux_Email = email.value;
+    var aux_Nome = nome.value;
+    var aux_Sobrenome = sobrenome.value;
+    var aux_Nick = nick.value;
+    var aux_Telefone = telefone.value;
 
-    const novoRegistro = await db.collection('pessoas').add({
-        nome: aux_Nome,
+    var aux_Cep = cep.value;
+    var aux_Estado = estado.value;
+    var aux_Endereco = endereco.value;
+    var aux_Cidade = cidade.value;
+    var aux_Mensagem = mensagem.value;
+
+    const novoRegistro = await db.collection('cabeludos').add({
         email: aux_Email,
-        telefone: aux_Telefone
+        nome: aux_Nome,
+        sobrenome: aux_Sobrenome,
+        nick: aux_Nick,
+        telefone: aux_Telefone,
+
+        cep: aux_Cep,
+        estado: aux_Estado,
+        endereco: aux_Endereco,
+        cidade: aux_Cidade,
+        mensagem: aux_Mensagem
     })
 
-    if (nome.value != "" && email.value != "" && telefone.value != "") {
-        var novo = new Pessoa(nome.value, email.value, telefone.value);
+    if (email.value != "" && nome.value != "" && sobrenome.value != "" && telefone.value != "" &&
+        cep.value != "" && estado.value != "" && endereco.value != "" && cidade.value != "") {
+
+        var novo = new Pessoa(email.value, nome.value, sobrenome.value, nick.value, telefone.value,
+            cep.value, estado.value, endereco.value, cidade.value, mensagem.value);
         listaPessoas.push(novo);
-        document.getElementById("nomeid").value = "";
-        document.getElementById("telefoneid").value = "";
+
         document.getElementById("emailid").value = "";
+        document.getElementById("firstid").value = "";
+        document.getElementById("lastid").value = "";
+        document.getElementById("nickid").value = "";
+        document.getElementById("numberid").value = "";
+
+        document.getElementById("zipid").value = "";
+        document.getElementById("regionid").value = "";
+        document.getElementById("addid").value = "";
+        document.getElementById("cityid").value = "";
+        document.getElementById("messageid").value = "";
 
         alert("Dados cadastrados com sucesso!");
 
@@ -57,20 +105,36 @@ async function Cadastrar() {
         elementos++;
         pagina = -1;
     } else {
-        alert("TODOS OS CAMPOS SÃO OBRIGATÓRIOS");
+        alert("Preencha os campos obrigatórios!");
     }
 }
 
 function Exibir() {
-    document.getElementById("nomeid").value = listaPessoas[pagina].nome;
-    document.getElementById("telefoneid").value = listaPessoas[pagina].telefone;
     document.getElementById("emailid").value = listaPessoas[pagina].email;
+    document.getElementById("firstid").value = listaPessoas[pagina].nome;
+    document.getElementById("lastid").value = listaPessoas[pagina].sobrenome;
+    document.getElementById("nickid").value = listaPessoas[pagina].nick;
+    document.getElementById("numberid").value = listaPessoas[pagina].number;
+
+    document.getElementById("zipid").value = listaPessoas[pagina].email;
+    document.getElementById("regionid").value = listaPessoas[pagina].nome;
+    document.getElementById("addid").value = listaPessoas[pagina].sobrenome;
+    document.getElementById("cityid").value = listaPessoas[pagina].nick;
+    document.getElementById("messageid").value = listaPessoas[pagina].number;
 }
 
 function Limpar() {
-    document.getElementById("nomeid").value = "";
-    document.getElementById("telefoneid").value = "";
     document.getElementById("emailid").value = "";
+    document.getElementById("firstid").value = "";
+    document.getElementById("lastid").value = "";
+    document.getElementById("nickid").value = "";
+    document.getElementById("numberid").value = "";
+
+    document.getElementById("zipid").value = "";
+    document.getElementById("regionid").value = "";
+    document.getElementById("addid").value = "";
+    document.getElementById("cityid").value = "";
+    document.getElementById("messageid").value = "";
 }
 
 function Anterior() {
@@ -90,12 +154,26 @@ function AtualizarDadosCadastrados() {
         var pessoa = listaPessoas[i];
         var infoPessoa = document.createElement("p");
         infoPessoa.textContent =
-            "Nome: " +
+            "Email: " +
+            pessoa.email +
+            ", Nome: " +
+            pessoa.nome +
+            ", Sobrenome: " +
+            pessoa.sobrenome +
+            ", Nick: " +
             pessoa.nome +
             ", Telefone: " +
             pessoa.telefone +
-            ", Email: " +
-            pessoa.email;
+            ", CEP: " +
+            pessoa.cep +
+            ", Estado: " +
+            pessoa.estado +
+            ", Endereço: " +
+            pessoa.endereco +
+            ", Cidade: " +
+            pessoa.cidade +
+            ", Mensagem: " +
+            pessoa.mensagem;
         dadosCadastrados.appendChild(infoPessoa);
     }
 }
@@ -110,14 +188,20 @@ function CriarTabela() {
     var colunaCabecalho2 = document.createElement("th");
     var colunaCabecalho3 = document.createElement("th");
     var colunaCabecalho4 = document.createElement("th");
-    colunaCabecalho1.textContent = "Nome";
-    colunaCabecalho2.textContent = "Telefone";
-    colunaCabecalho3.textContent = "Email";
-    colunaCabecalho4.textContent = "Ação";
+    var colunaCabecalho5 = document.createElement("th");
+    var colunaCabecalho6 = document.createElement("th");
+    colunaCabecalho1.textContent = "Email";
+    colunaCabecalho2.textContent = "Nome";
+    colunaCabecalho3.textContent = "Sobrenome";
+    colunaCabecalho4.textContent = "Nick";
+    colunaCabecalho5.textContent = "Telefone";
+    colunaCabecalho6.textContent = "Ação";
     linhaCabecalho.appendChild(colunaCabecalho1);
     linhaCabecalho.appendChild(colunaCabecalho2);
     linhaCabecalho.appendChild(colunaCabecalho3);
     linhaCabecalho.appendChild(colunaCabecalho4);
+    linhaCabecalho.appendChild(colunaCabecalho5);
+    linhaCabecalho.appendChild(colunaCabecalho6);
     tabela.appendChild(linhaCabecalho);
 
     // Cria as linhas de dados
@@ -128,17 +212,42 @@ function CriarTabela() {
         var coluna2 = document.createElement("td");
         var coluna3 = document.createElement("td");
         var coluna4 = document.createElement("td");
-        coluna1.textContent = pessoa.nome;
-        coluna2.textContent = pessoa.telefone;
-        coluna3.textContent = pessoa.email;
+        var coluna5 = document.createElement("td");
+        var coluna6 = document.createElement("td");
+        var coluna7 = document.createElement("td");
+        var coluna8 = document.createElement("td");
+        var coluna9 = document.createElement("td");
+        var coluna10 = document.createElement("td");
+        var coluna12 = document.createElement("td");
+        var coluna11 = document.createElement("td");
+        coluna1.textContent = pessoa.email;
+        coluna2.textContent = pessoa.nome;
+        coluna3.textContent = pessoa.sobrenome;
+        coluna4.textContent = pessoa.nick;
+        coluna5.textContent = pessoa.telefone;
+
+        coluna6.textContent = pessoa.cep;
+        coluna7.textContent = pessoa.estado;
+        coluna8.textContent = pessoa.endereco;
+        coluna9.textContent = pessoa.cidade;
+        coluna10.textContent = pessoa.mensagem;
+
         var botaoRemover = document.createElement("button");
         botaoRemover.textContent = "Remover";
         botaoRemover.setAttribute("onclick", "RemoverElemento(" + i + ")");
-        coluna4.appendChild(botaoRemover);
+        coluna11.appendChild(botaoRemover);
+
         linha.appendChild(coluna1);
         linha.appendChild(coluna2);
         linha.appendChild(coluna3);
         linha.appendChild(coluna4);
+        linha.appendChild(coluna5);
+        linha.appendChild(coluna6);
+        linha.appendChild(coluna7);
+        linha.appendChild(coluna8);
+        linha.appendChild(coluna9);
+        linha.appendChild(coluna10);
+        linha.appendChild(coluna11);
         tabela.appendChild(linha);
     }
 }
