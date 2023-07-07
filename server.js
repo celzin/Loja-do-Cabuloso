d6b3130a1a799ef566908d1e7f39ae9acef49488
Celso Vinícius <celsovinicius4@gmail.com>
2023-07-07 15:59:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.post('/stripe-checkout', async (req, res) => {
     res.json(session.url);
 });
 
-app.listen(3003, () => {
+app.listen(3000, () => {
     console.log("listening on port 3000;");
 });
